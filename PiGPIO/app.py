3850d279350eacc4943e036e9b6e864c2d5b353b
ZaKa79 <115881704+ZaKa79@users.noreply.github.com>
2023-04-24 23:49:04 +0200

    Add files via upload

---
+++ PiGPIO/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+import pigpio
+
+pi = pigpio.pi()
+
+app = Flask(__name__)
+
+BUTTON_GPIO_PIN = 20
+
+@app.route('/')
+def index():
+    if pi.read(BUTTON_GPIO_PIN) == 0:
+        return 'TRYKKET'
+    else:
+        return 'SLUPPET'
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
